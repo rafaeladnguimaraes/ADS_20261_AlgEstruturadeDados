2997c79004caf005364db60a225206fac448b3bf
RAFAELA DO NASCIMENTO GUIMARAES <60082172099@senacrs.edu.br>
2026-06-03 11:36:52 -0300

    iniciando desafio

---
+++ aula_10/algor_busca.py
@@ -0,0 +1,15 @@
+class Busca:
+    def __init__ (self, dado):
+        self.dado = dado
+        self.vet = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+        self.tam = len(self.vet)
+    
+    def b_seq(self):
+        i = 0
+        while i < self.tam:
+            
+            
+
+
+
+
